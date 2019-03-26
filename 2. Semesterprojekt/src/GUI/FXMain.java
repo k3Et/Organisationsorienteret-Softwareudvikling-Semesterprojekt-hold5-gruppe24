ce333c9d5c0168caddeef9d62e434d8eb3c701cc
TempestRules <43045051+TempestRules@users.noreply.github.com>
2019-03-26 14:24:47 +0100

    LoginFXML Test

--- 2. Semesterprojekt/src/GUI/FXMain.java
+++ 2. Semesterprojekt/src/GUI/FXMain.java
@@ -15,7 +15,7 @@ public class FXMain extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("LoginFXML.fxml"));
 
         Scene scene = new Scene(root);
 
