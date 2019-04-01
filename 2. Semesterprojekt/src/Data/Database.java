0bcd98e7430537c9588eb15f8c50fbb4e283de47
TempestRules <vielm18@student.sdu.dk>
2019-04-01 22:25:25 +0200

    Login Verification
    Database implemented.
    Login verification through database implemented.

--- 2. Semesterprojekt/src/Data/Database.java
+++ 2. Semesterprojekt/src/Data/Database.java
@@ -1,9 +1,57 @@
 package Data;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 /**
  *
  * @author Victor
  */
 public class Database {
+
+    Connection con = null;
+    PreparedStatement ps = null;
+    ResultSet rs = null;
+
+    String url = "jdbc:postgresql://balarama.db.elephantsql.com:5432/qtbrqkid";
+    String Username = "qtbrqkid";
+    String Password = "ju1tuAMRQjWkEU-_Pe-5gHMzyNQfm7qa";
+
+    public boolean verifyLogin(String username, String password) {
+
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException ex) {
+            System.out.println(ex);
+        }
+
+        try {
+            con = DriverManager.getConnection(url, Username, Password);
+
+            ps = con.prepareStatement("SELECT * FROM Login WHERE username = ? and password = ?");
+
+            ps.setString(1, username);
+            ps.setString(2, password);
+
+            rs = ps.executeQuery();
+
+            if (rs.next()) {
+                System.out.println("Query Accepeted");
+                return true;
+            } else {
+                System.out.println("Query Failed");
+            }
+            ps.close();
+            rs.close();
+            con.close();
+        } catch (SQLException ex) {
+            System.out.println(ex);
+        }
+
+        return false;
+    }
 
 }
--- 2. Semesterprojekt/src/GUI/LoginFXMLController.java
+++ 2. Semesterprojekt/src/GUI/LoginFXMLController.java
@@ -1,5 +1,6 @@
 package GUI;
 
+import Data.Database;
 import java.net.URL;
 import java.util.Random;
 import java.util.ResourceBundle;
@@ -31,12 +32,15 @@ public class LoginFXMLController implements Initializable {
     @FXML
     private Label userNameLabel;
     @FXML
+    private Label resultLabel;
+    @FXML
     private Button loginButton;
     @FXML
     private ImageView NoTiBoImage;
 
     Random r = new Random();
     Image[] loginImages;
+    Database ds = new Database();
 
     /**
      * Initializes the controller class.
@@ -58,11 +62,21 @@ public class LoginFXMLController implements Initializable {
     @FXML
     private void handlePassWordFIeldAction(ActionEvent event) {
         //Create verification with SQL database here:
+        if (ds.verifyLogin(userNameField.getText(), passWordField.getText())) {
+            System.out.println("DET HELE VIRKER GODT ARBEJDE");
+        } else {
+            resultLabel.setText("Loginfejl. Forkert Brugernavn eller Password.");
+        }
     }
 
     @FXML
     private void handleLoginButtonAction(ActionEvent event) {
         //Create verification with SQL database here:
+        if (ds.verifyLogin(userNameField.getText(), passWordField.getText())) {
+            System.out.println("Login Virker");
+        } else {
+            resultLabel.setText("Loginfejl. Forkert Brugernavn eller Password.");
+        }
     }
 
 }
