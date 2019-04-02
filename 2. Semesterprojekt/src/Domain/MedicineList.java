66687723b1bfaf9feefb0fcceff08251cdbbcbef
k3Et <yourk3Et@gmail.com>
2019-04-02 11:15:23 +0200

    Medicinskema (#5)
    * Create Medicine.java
    
    Oprettet Medicine klassen, skal videre udvikles på.
    
    * Splittet Medicine and MedicineList
    
    Gjort at medicine og MedicineList er splittet, da det ikke skulle være sammen til at starte med.

---
+++ 2. Semesterprojekt/src/Domain/MedicineList.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Domain;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Yourk
+ */
+public class MedicineList {
+ 
+    private ArrayList<Medicine> medicineList;
+    private File medicineFile;
+    private Medicine medicine;
+    
+    public MedicineList(){
+        medicineList = new ArrayList<>();
+        medicineFile = new File("medicineList.txt");
+    }
+    
+    
+    private void addMedicine(Medicine medicine){
+        medicineList.add(medicine);
+    }
+    
+    private void removeMedicine(Medicine medicine){
+        medicineList.remove(medicine);
+    }
+    
+    public ArrayList getMedicineList(){
+        try(Scanner sc = new Scanner(medicineFile)){
+            while (sc.hasNext()) {
+                String temp = sc.next();
+                String[] tempArray = temp.split("[:]");
+                medicineList.add(new Medicine(tempArray[0], Double.parseDouble(tempArray[1])));
+            }
+        } catch (FileNotFoundException ex) {
+            System.out.println(ex);
+        }
+        return medicineList;
+    }
+    
+    public void saveMedicineList(){
+        try(FileWriter fw = new FileWriter(medicineFile)) {
+            for (Medicine medicine : medicineList){
+                fw.write(medicine.getName()+":"+medicine.getDose()+"\n");
+            }
+        } catch (IOException ex) {
+            System.out.println(ex);
+        }
+    }
+}
