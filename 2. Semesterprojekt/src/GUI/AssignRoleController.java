60bef6c02eb79d9042ae77412142771329fc82eb
rayou18 <43138817+rayou18@users.noreply.github.com>
2019-04-09 14:00:55 +0200

    SceneSkifte på plads, mangler tilføjelse af ny bruger

--- 2. Semesterprojekt/src/GUI/AssignRoleController.java
+++ 2. Semesterprojekt/src/GUI/AssignRoleController.java
@@ -5,16 +5,24 @@ import Domain.ListOfUsers;
 import Domain.Role;
 import Domain.Roles.Employee;
 import Domain.User;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -46,6 +54,8 @@ public class AssignRoleController implements Initializable {
 
     private User selectedUser;
     private Role selectedRole;
+
+    private SceneHandler sh = new SceneHandler();
 
     /**
      * Initializes the controller class.
@@ -84,11 +94,10 @@ public class AssignRoleController implements Initializable {
         selectedRole = chooseRoleList.getSelectionModel().getSelectedItem();
         System.out.println(selectedRole);
 
-       
         if (selectedRole != null) {
             addRoleToUser(selectedUser, selectedRole);
         }
-         loadUserInfo(selectedUser);
+        loadUserInfo(selectedUser);
 
     }
 
@@ -97,7 +106,7 @@ public class AssignRoleController implements Initializable {
         s = new Scanner(selectedUser.toStringAll());
 
         stringHolder = s.nextLine();
-        
+
         String[] infoStringArray = stringHolder.split(";");
 
         for (int i = 0; i < infoStringArray.length; i++) {
@@ -125,9 +134,6 @@ public class AssignRoleController implements Initializable {
         chooseUserList.getSelectionModel().clearSelection();
     }
 
-
-
-
     @FXML
     private void sletRolleBtnHandler(ActionEvent event) {
         selectedUser = chooseUserList.getSelectionModel().getSelectedItem();
@@ -136,5 +142,9 @@ public class AssignRoleController implements Initializable {
         loadUserInfo(selectedUser);
     }
 
-}
+    @FXML
+    private void addNewUserBtn(ActionEvent event) {
+        sh.setNewScene("/GUI/FXML/NewUser.fxml");
+    }
 
+}
--- 2. Semesterprojekt/src/GUI/FXMain.java
+++ 2. Semesterprojekt/src/GUI/FXMain.java
@@ -4,6 +4,9 @@ import Domain.ListOfUsers;
 import Domain.Role;
 import Domain.Roles.Employee;
 import Domain.User;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.application.Application;
 import static javafx.application.Application.launch;
 import javafx.fxml.FXMLLoader;
@@ -17,30 +20,31 @@ import javafx.stage.Stage;
  */
 public class FXMain extends Application {
 
-    
+    private SceneHandler sh = new SceneHandler();
+
+    public FXMain() {
+
+    }
+
+    private Stage primaryStage;
+
     @Override
-    public void start(Stage stage) throws Exception {
+    public void start(Stage stage) {
 
-        Parent root = FXMLLoader.load(getClass().getResource("/GUI/FXML/AssignRole.fxml"));
+        primaryStage = stage;
+        sh.setCurrentStage(primaryStage);
+        sh.setNewScene("/GUI/FXML/AssignRole.fxml");
+      
 
-        Scene scene = new Scene(root);
-
-        stage.setScene(scene);
-        stage.setTitle("NoTiBo - Noter til Dagbøger");
-       // stage.setMaximized(true);
-        stage.show();
+        // stage.setMaximized(true);
     }
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        AssignRoleController ac = new AssignRoleController();
-
-      
-      
         launch(args);
-       
+
     }
 
 }
