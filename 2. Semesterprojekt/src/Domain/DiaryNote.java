44d011f51c71c283f6c2628a835b32e3325539ae
AntiElysium <patrick@pilsby.dk>
2019-05-07 11:09:12 +0200

    Update DiaryNote.java

--- 2. Semesterprojekt/src/Domain/DiaryNote.java
+++ 2. Semesterprojekt/src/Domain/DiaryNote.java
@@ -9,11 +9,11 @@ import java.util.Date;
 public class DiaryNote {
 
     private String note;
-    private String employee;
-    private String time;
-    private String editedTime;
-    private Date date;
-    private Medicine medicine;
+//    private String employee;
+//    private String time;
+//    private String editedTime;
+//    private Date date;
+//    private Medicine medicine;
 
     public DiaryNote(String note) {
         this.note = note;
@@ -24,25 +24,25 @@ public class DiaryNote {
         return note;
     }
 
-    public String getEmployee() {
-        return employee;
-    }
-
-    public String getTime() {
-        return time;
-    }
-
-    public String getEditedTime() {
-        return editedTime;
-    }
-
-    public Date getDate() {
-        return date;
-    }
-
-    public Medicine getMedicine() {
-        return medicine;
-    }
+//    public String getEmployee() {
+//        return employee;
+//    }
+//
+//    public String getTime() {
+//        return time;
+//    }
+//
+//    public String getEditedTime() {
+//        return editedTime;
+//    }
+//
+//    public Date getDate() {
+//        return date;
+//    }
+//
+//    public Medicine getMedicine() {
+//        return medicine;
+//    }
 
     public void setNote(String note) {
         this.note = note;
